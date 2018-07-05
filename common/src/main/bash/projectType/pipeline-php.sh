7e01628a3540976fd1f4d044f581fdaf4786c064
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-07-05 14:26:30 +0200

    Made PHP source based by default

--- common/src/main/bash/projectType/pipeline-php.sh
+++ common/src/main/bash/projectType/pipeline-php.sh
@@ -156,3 +156,5 @@ function installPhpIfMissing() {
 		"${APT_BIN}" -y install php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml  > /dev/null 2>&1
 	fi
 }
+
+ARTIFACT_TYPE="${SOURCE_ARTIFACT_TYPE_NAME}"
